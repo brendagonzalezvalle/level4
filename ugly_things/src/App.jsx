3c6cc66625c2f032d72311f87f9fd4a8da089adc
brendagonzalezvalle <brendagonzalezvalle@gmail.com>
2023-11-02 20:31:59 -0600

    created components

--- ugly_things/src/App.jsx
+++ ugly_things/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Header'
+import List from './List'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <h1>App Test</h1>
+      <Header/>
+      <List/>
     </>
   )
 }
